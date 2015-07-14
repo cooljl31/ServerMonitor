a57a1fa063bc1496f60821b8874dbb65bfdbfb20
Ashley Davis <ashley.davis@realseriousgames.com>
2015-07-14 17:27:38 +1000

    Now doing a HTTP GET to check the status of hosts every 5 minutes.

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 
+var request = require('request-promise');
+
+var hostsToCheck = [
+	'http://www.google.com',
+];
+
 
 var cronJob = require('cron').CronJob;
 var job = new cronJob({
@@ -6,6 +12,32 @@ var job = new cronJob({
     onTick: function() { 
     	console.log('Running!');
     	console.log(new Date());
+
+    	hostsToCheck.forEach(function (hostUrl) {
+    		try {
+	    		request({
+	    				method: 'GET',
+	    				uri: hostUrl,
+	    				resolveWithFullResponse: true,
+	    			})
+	    			.then(function (result) {
+	    				if (result.statusCode === 200) {
+	    					console.log(hostUrl + ' is ok.');
+	    				}
+	    				else {
+	    					console.error('Problem with ' + hostUrl + ', returned status code: ' + result.statusCode);	
+	    				}
+	    			})
+	    			.catch(function (err) {
+	    				console.error('An error occurred checking ' + hostUrl);
+	    				console.error(err.stack);
+	    			});
+	    	}
+	    	catch (ex) {
+				console.error('An exception occurred checking ' + hostUrl);
+				console.error(ex.stack);
+	    	}
+    	});
     }, 
     start: true,
 });
