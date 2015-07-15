a2d533929e53c73e8d5f52c8a2e56a233cf280c2
Ashley Davis <ashley.davis@realseriousgames.com>
2015-07-15 12:59:58 +1000

    Added simple deployment script.

---
+++ Deploy/server_restart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Note: if you update this file and deploy, this script will be invoke on the serer.
+
+D=$(date +"%m_%d_%Y")
+LOG=./logs/restart_$D.log
+echo === $D === > $LOG
+pm2 stop all >> $LOG
+pm2 start ./server.js >> $LOG
+pm2 save
+echo Restart after commit $HG_NODE >> $LOG
