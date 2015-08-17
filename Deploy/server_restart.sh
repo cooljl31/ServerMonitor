75fdebb393501243e15bf30b6e9f495deaa32685
Ashley Davis <ashley.davis@realseriousgames.com>
2015-08-17 14:43:03 +1000

    Updates to deployment script.

--- Deploy/server_restart.sh
+++ Deploy/server_restart.sh
@@ -2,9 +2,9 @@
 # Note: if you update this file and deploy, this script will be invoke on the server.
 
 D=$(date +"%m_%d_%Y")
-LOG=./logs/restart_$D.log
+LOG=$(pwd)/logs/restart_$D.log
 echo === $D === > $LOG
-pm2 stop all >> $LOG
+pm2 delete all >> $LOG
 pm2 start ./server.js >> $LOG
 pm2 save
 echo Restart after commit $HG_NODE >> $LOG
